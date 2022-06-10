5f513ad9c5658bc3bf530c8c063868686785af16
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-11 01:51:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, received) {
+    
+    const result = drivers.filter(driver=> driver.toUpperCase() === received.toUpperCase());
+    if(result){
+        return result
+    }
+
+}
+function fuzzyMatch(drivers, received) {
+    const result = drivers.filter(drivers=>drivers.toLowerCase().indexOf(received.toLowerCase())===0)
+    if(result){
+        return result
+    }
+}
+function matchName(drivers, received) {
+    const result = drivers.filter(driver=> driver.name === received);
+    if(result){
+        return result
+    }
+}
